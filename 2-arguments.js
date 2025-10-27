77128ce5b2e2ac0e9fcdbf9ff514f78b19634ea0
Asenathi Sotshintshi <sotshintshiasenathi@gmail.com>
2025-10-28 01:53:59 +0200

    Add 2-arguments.js - print messages based on number of arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,5 @@
-const args = Process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (args.length ===0){
-    console.log("No argument");
-} else if (args.length === 1) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
+if (args.length === 0) console.log("No argument");
+else if (args.length === 1) console.log("Argument found");
+else console.log("Arguments found");
