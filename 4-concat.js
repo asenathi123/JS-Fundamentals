6dc4f4ea15b4468e01f88520b5c9e9803accbf5f
Asenathi Sotshintshi <sotshintshiasenathi@gmail.com>
2025-10-28 02:01:44 +0200

    Add 4-concat.js - print two arguments in a sentence

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,4 @@
-const arg1 = ProcessingInstruction.argv[2];
-const arg2 =ProcessingInstruction.argv[3];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
 console.log(`${arg1} is ${arg2}`);
